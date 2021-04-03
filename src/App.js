b701d03692b11c23dbb5ccc16dfadf537a264f65
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-03 18:05:46 -0500

    primera entrega

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-
+import React, { Component } from 'react';
 import './App.css';
+import tasks from "./sample/task.json";
 
+class App extends Component {
+
+  state = {
+    tasks: tasks
+  }
+
+  render(){
+    return <div>
+
+    </div>
+  }
+}
 
 
 export default App;
