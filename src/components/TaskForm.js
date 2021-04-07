5f624f33759151e37b4e75e8c9e1cd225c119d8c
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-06 20:25:42 -0500

    segunda entrega

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 export default class TaskForm extends Component {
+    state = {
+        title: '',
+        description: ''
+    }
 
     //esto es para evitar que se refresque la pagina
     onSubmit = e => { //e es un parametro,que en este caso significa evento puede ser cualquier nombre o letra, como el parametro solo tiene un valor, no es necesario poner los parentecis
@@ -15,10 +19,18 @@ export default class TaskForm extends Component {
     render () {
         return (
             <form onSubmit={this.onSubmit}> {/*onsubmit es para que no se refresque la pagina cada que le damos enviar */}
-                <input type="text" placeholder="Write a Task" onChange={this.onChange}/> {/*placeholder sirve para agregar un campo en el cual se puede escribir */}
+                <input 
+                    type="text" 
+                    placeholder="Write a Task"
+                    onChange={this.onChange} 
+                    value={this.state.title}/> {/*placeholder sirve para agregar un campo en el cual se puede escribir */} 
                 <br/>
                 <br/>
-                <textarea placeholder="Write a Description" onChange={this.onChange}></textarea> {/*onchance  es para que se escuche lo que se escriba en los dos input, y se escucha por medio de e.target.value*/}
+                <textarea 
+                    placeholder="Write a Description" 
+                    onChange={this.onChange} 
+                    value={this.state.description}>{/*onchance  es para que se escuche lo que se escriba en los dos input, y se escucha por medio de e.target.value*/}
+                </textarea> 
                 <input type="submit"/>
             </form>
         )
